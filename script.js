42508c47e799b0a9b66ebcf1512ecc94af5e3f55
Milos Dobrijevic <dobry95@gmail.com>
2022-04-04 20:04:48 +0200

    update

--- script.js
+++ script.js
@@ -17,34 +17,14 @@ window.addEventListener('load', function () {
 		mobileMenuItems[i].addEventListener('click', function () {
 			mobileMenu.style.display = 'none';
 		});
-	}
+	};
 
 
 	// logika za mape
-	var map = L.map('map').setView([51.505, -0.09], 13);
+	let myMap = L.map('map');
+	myMap.setView([37.974, 23.72], 13);
+
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
-
-	L.marker([51.5, -0.09]).addTo(map)
-		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-		.openPopup();
-
-
-	// ZA PRELAZ SA JEDNE SEKCIJE NA DRUGU
-	// let heroSection = getElementById('hero-section');
-	// let heroSectionHeight = heroSection.clientHeight;
-	// let header = document.getElementById('header');
-
-	// document.addEventListener('scroll', function () {
-	// 	if (document.documentElement.scrollTop > heroSectionHeight) {
-	// 		if (!header.classList.contains('solid-header')) {
-	// 			header.classList.add('solid-header');
-	// 		}
-	// 	} else {
-	// 		if (header.classList.contains('solid-header')) {
-	// 			header.classList.remove('solid-header');
-	// 		}
-	// 	}
-	// });
+	}).addTo(myMap);
 });
